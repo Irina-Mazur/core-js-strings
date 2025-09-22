0141766d95d08541e42a92765ce5c8966af3c57e
Irina-Mazur <irin.mazur@gmail.com>
2025-09-22 22:20:13 +0300

    feat: add solution task nineteen

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -322,8 +322,9 @@ function countVowels(str) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const filteredStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  return filteredStr === filteredStr.split('').reverse().join('');
 }
 
 /**

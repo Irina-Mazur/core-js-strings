9ec2c89f31d755936924587b1bd6c11a876228d0
Irina-Mazur <irin.mazur@gmail.com>
2025-09-22 21:19:47 +0300

    feat: add solution task eight

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -136,8 +136,11 @@ function removeTrailingWhitespaces(value) {
  *   repeatString('', 3) => ''
  *   repeatString('abc', -2) => ''
  */
-function repeatString(/* str, times */) {
-  throw new Error('Not implemented');
+function repeatString(str, times) {
+  if (typeof times !== 'number' || times < 0 || !Number.isFinite(times)) {
+    return '';
+  }
+  return str.repeat(times);
 }
 
 /**
